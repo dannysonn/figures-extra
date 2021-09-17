48706488d55772ad65fc92af73b9e55333a4c5ff
danila_kryuchkov <dannyson5@bk.ru>
2021-09-17 16:48:17 +0300

    Initial commit

---
+++ src/main/java/com/epam/rd/autotasks/figures/Triangle.java
@@ -0,0 +1,73 @@
+package com.epam.rd.autotasks.figures;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Triangle extends Figure {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    double AB;
+    double AC;
+    double BC;
+
+    public Triangle(Point a, Point b, Point c) {
+        if (a == null){
+            throw new IllegalArgumentException();
+        }
+
+        if (b == null){
+            throw new IllegalArgumentException();
+        }
+
+        if (c == null){
+            throw new IllegalArgumentException();
+        }
+
+        AB = Math.sqrt(Math.pow((b.getX() - a.getX()), 2) + Math.pow((b.getY() - a.getY()), 2));
+        AC = Math.sqrt(Math.pow((c.getX() - a.getX()), 2) + Math.pow((c.getY() - a.getY()), 2));
+        BC = Math.sqrt(Math.pow((c.getX() - b.getX()), 2) + Math.pow((c.getY() - b.getY()), 2));
+
+        if ((c.getX() - a.getX()) / (b.getX() - a.getX()) == (c.getY() - a.getY()) / (b.getY() - a.getY())){
+            throw new IllegalArgumentException();
+        }
+
+
+        if (area() == 0){
+                throw new IllegalArgumentException();
+            }
+
+
+
+        else {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+        }
+
+
+    }
+
+    public double area() {
+        double s = 0.5 * (AB + AC + BC);
+        double area = Math.sqrt(s*(s-AB)*(s-AC)*(s-BC));
+
+        return area;
+    }
+
+
+    @Override
+    public Point centroid() {
+        double x = 1.0/3.0 * (a.getX() + b.getX() + c.getX());
+        double y = 1.0/3.0 * (a.getY() + b.getY() + c.getY());
+        return new Point(x, y);
+    }
+
+    @Override
+    public boolean isTheSame(Figure figure) {
+        return false;
+    }
+}
+
---
+++ src/test/java/com/epam/rd/autotasks/figures/Main.java
@@ -0,0 +1,18 @@
+package com.epam.rd.autotasks.figures;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+import static java.lang.Math.sqrt;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(sqrt(3) * sqrt(3));
+
+
+
+
+    }
+}
